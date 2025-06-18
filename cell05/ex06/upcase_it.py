15b1aa5e53b249eede109709323380c7de3e7a4b
saipin13 <68030293@kmitl.ac.th>
2025-06-18 08:27:22 +0000

    upcase_it.py

---
+++ cell05/ex06/upcase_it.py
@@ -0,0 +1,6 @@
+import sys #ต้องrunในbashผลถึงจะออกตามโจทย์
+
+if len(sys.argv) == 2:
+   print(sys.argv[1].upper())
+else:
+   print("none")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
